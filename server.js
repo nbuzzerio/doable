3985bf651a48120b95b196f5064700434648c05c
nbuzzerio <nick.buzzerio@hotmail.com>
2025-01-03 17:28:49 -0500

    separating client from server

--- server.js
+++ server.js
@@ -12,11 +12,11 @@ const app = express();
 const port = process.env.PORT || 7000;
 
 // Serve static files from the 'dist' folder
-app.use(express.static(__dirname + "/dist"));
+app.use(express.static(__dirname + "/client/dist"));
 
 // Define a catch-all route to serve 'index.html'
 app.get("*", (req, res) => {
-  res.sendFile(__dirname + "/dist/index.html");
+  res.sendFile(__dirname + "/client/dist/index.html");
 });
 
 // Start the server
